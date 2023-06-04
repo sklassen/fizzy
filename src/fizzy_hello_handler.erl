7739d27dcfa6614c81c3c7c64d57a277891c30cc
sklassen <none>
2023-06-04 12:48:26 +0800

    Added the Name to the greeting

--- src/fizzy_hello_handler.erl
+++ src/fizzy_hello_handler.erl
@@ -3,11 +3,19 @@
 
 -export([init/2]).
 
+init(#{bindings := #{name := Name}}=Req0, State) ->
+    io:format("Got Request ~p~n",[Req0]),
+    io:format("Got State ~p~n",[State]),
+    Req = cowboy_req:reply(200, #{
+        <<"content-type">> => <<"text/plain">>
+    }, <<"Fizzy Says, Hello ",Name/binary>>, Req0),
+    {ok, Req, State};
+
 init(Req0, State) ->
     io:format("Got Request ~p~n",[Req0]),
     io:format("Got State ~p~n",[State]),
     Req = cowboy_req:reply(200, #{
         <<"content-type">> => <<"text/plain">>
-    }, <<"Fizzy Says, Hello World">>, Req0),
+    }, <<"Fizzy Says, Hello Unknown">>, Req0),
     {ok, Req, State}.
 
