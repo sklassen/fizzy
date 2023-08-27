7d59acd327489257c9d5499bc42f6a27438af7e1
sklassen <none>
2023-08-28 01:32:54 +0800

    shh'd the compiler

--- src/fizzy_cli.erl
+++ src/fizzy_cli.erl
@@ -1,15 +1,15 @@
 -module(fizzy_cli).
 -export([hello/1]).
 
-hello(Text) ->
+hello(_Text) ->
   {ok,Port} = application:get_env(fizzy,port),
   {ok,ConnPid} = gun:open({127,0,0,1}, Port),
 
   StreamRef = gun:get(ConnPid, "/"),
   case gun:await(ConnPid, StreamRef) of
-    {response, fin, Status, Headers} ->
+    {response, fin, _Status, _Headers} ->
       no_data;
-    {response, nofin, Status, Headers} ->
+    {response, nofin, _Status, _Headers} ->
       {ok, Body} = gun:await_body(ConnPid, StreamRef),
       Body
   end.
